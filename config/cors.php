1cd9a360fa87141324dbe9c42d7a3d5f1ff01972
NujanSitaula <nujan.shotcoder@gmail.com>
2026-03-25 12:58:31 +0545

    Updated cores for shufflee

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,12 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [],
-    'allowed_origins_patterns' => [
-        '#^https?://[a-z0-9-]+\.35\.225\.205\.38\.sslip\.io$#',
+    'allowed_origins' => [
+        'https://shufflee.co',
+        'https://www.shufflee.co',
+        'https://admin.shufflee.co',
     ],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
